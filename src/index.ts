96f1fd336ce444ff6084e952223f1385ba8d5cec
Gihan-1994 <nggp1994@gmail.com>
2025-03-15 03:26:51 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+// Root route
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Welcome to the Greeting API! The task is to implement /greet-{name} to get a personalized greeting.' });
+});
+
+// Greet route
+app.get('/greet-:name', (req: Request, res: Response) => {
+    const name = req.params.name.substring(0, 3); // Get the first 3 letters of the name
+    const message = `Hello! I am ${name}.. Cheers 🥂`;
+    res.json({ message });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
